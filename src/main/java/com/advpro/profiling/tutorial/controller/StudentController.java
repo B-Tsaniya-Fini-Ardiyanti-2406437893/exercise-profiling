0e482ebcb372d8f297af6c78df7357c2976cd541
unknown <fini.ardiyanti@gmail.com>
2026-04-26 21:35:26 +0700

    [Refactoring] - Optimizing method getAllStudentNames and getHighestGpaStudent

--- src/main/java/com/advpro/profiling/tutorial/controller/StudentController.java
+++ src/main/java/com/advpro/profiling/tutorial/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.advpro.profiling.tutorial.controller;
 
 import com.advpro.profiling.tutorial.model.Student;
 import com.advpro.profiling.tutorial.model.StudentCourse;
-import com.advpro.profiling.tutorial.service.DataSeedService;
 import com.advpro.profiling.tutorial.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author muhammad.khadafi
  */
-
 @RestController
 public class StudentController {
 
@@ -23,19 +20,20 @@ public class StudentController {
     private StudentService studentService;
 
     @GetMapping("/all-student")
-    public ResponseEntity<String> seedStudents() {
+    public ResponseEntity<String> allStudents() {
         List<StudentCourse> studentCourses = studentService.getAllStudentsWithCourses();
         return ResponseEntity.ok(studentCourses.toString());
     }
+
     @GetMapping("/highest-gpa")
     public ResponseEntity<String> highestGpa() {
-        Optional<Student> studentWithHighestGpa = studentService.findStudentWithHighestGpa();
-        return ResponseEntity.ok(studentWithHighestGpa.get().toString());
+        Student studentWithHighestGpa = studentService.getHighestGpaStudent();
+        return ResponseEntity.ok(studentWithHighestGpa.toString());
     }
+
     @GetMapping("/all-student-name")
     public ResponseEntity<String> allStudentName() {
         String joinedStudentNames = studentService.joinStudentNames();
         return ResponseEntity.ok(joinedStudentNames);
     }
 }
-
